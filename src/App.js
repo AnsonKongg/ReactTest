7f882a5847f04b77009a894a2a88afa53ac41eea
Anson Kong <ansonkong1992@gmail.com>
2021-03-28 21:52:06 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const name = ''
+
+  return (
+    <div className="container">
+      <h1>React Interview</h1>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
